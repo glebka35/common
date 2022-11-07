29e6e122e24865aed388065125f364474f9884ec
Gleb Uvarkin <uvarking@yandex-team.ru>
2022-11-07 09:18:52 +0300

    Dynamic

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Common",
+            type: .dynamic,
             targets: ["Common"]),
     ],
     dependencies: [
